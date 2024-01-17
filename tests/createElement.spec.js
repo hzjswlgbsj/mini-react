4034cbf2fcbb561bcd6e85d4f37e72a5f3f4c9ae
Sixty <hiliulin@aliyun.com>
2024-01-17 11:08:15 +0800

    feat: 修改项目整体文件夹结构

--- tests/createElement.spec.js
+++ tests/createElement.spec.js
@@ -1,6 +1,16 @@
 import { describe, it, expect } from "vitest";
-import React from "../core/React.js";
-
+import React from "../src/core/React.js";
+// 模拟 requestIdleCallback
+globalThis.requestIdleCallback = (callback, options) => {
+  const start = Date.now();
+  return setTimeout(() => {
+    const elapsedTime = Date.now() - start;
+    callback({
+      timeRemaining: () => Math.max(0, options.timeout - elapsedTime),
+      didTimeout: options.timeout != null && elapsedTime >= options.timeout,
+    });
+  }, 1);
+};
 describe("createElement", () => {
   it("should return a vdom for element", () => {
     const element = React.createElement("div", { id: "app" }, "hello", "world");
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: "src/main.jsx", // 指定入口文件路径
+      },
+    },
+  },
+};
